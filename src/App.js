fb8a15ad79de277c0e805642cfad9f5d4531c6f9
KingNelx <tapiajonel606@gmail.com>
2022-11-03 13:49:11 +0800

    added new components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from './Components/Home';
-import AddForm from './Components/AddForm';
-import UserTables from './Components/UserTables.jsx'
-import AdminTables from './Components/AdminTables.jsx'
+
 const App = () => {
   return ( 
     <div>
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="addUser"  element={<AddForm />}/>
-            <Route path="UserTables" element={<UserTables />} />
-            <Route path="AdminTables" element={<AdminTables />} />
+           <Route />
           </Routes>
         </Router>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,13 @@
-import { Link } from "react-router-dom"
-import Column from "./Column";
-
 const Home = () => {
     return (
-        <div>
-            <nav className="navbar bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand">Full Stack Application</a>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-primary" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-        <Column />
+        <div className="top-nav">
+            <div className="left-title">
+                <h2>Full Stacp Application</h2>
+            </div>
+
+            <div className="right-title">
+
+            </div>
         </div>
     );
 }
