b1768b85508abe298cb815a8e2a8b0d8edc8d524
KingNelx <tapiajonel606@gmail.com>
2022-11-04 10:22:16 +0800

    added table for patient

---
+++ src/Components/Button.jsx
@@ -0,0 +1,11 @@
+const Button = () => {
+    return (
+        <div>
+            <button type="button" class="btn btn-outline-primary">VIEW</button>
+            <button type="button" class="btn btn-outline-danger">DELETE</button>
+            <button type="button" class="btn btn-outline-success">UPDATE</button>
+        </div>
+    );
+}
+
+export default Button;
--- src/Patients/PatientLog.jsx
+++ src/Patients/PatientLog.jsx
@@ -1,26 +1,33 @@
+import { Link } from "react-router-dom"
+import Button from "../Components/Button";
+
 const PatientLog = () => {
-    return ( 
-        <div className="container patient-top-head">
-            <table className="table-main">
+    return (
+        <div className="container mt-5 text-center">
+            <table class="table table-striped">
                 <thead>
-                    <tr className="table-head">
-                        <th>#</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Contact Number</th>
-                        <th>RHU</th>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <th>1</th>
-                    <td>Jonel</td>
-                    <td>Tapia</td>
-                    <td>09071205892</td>
-                    <td>Bato</td>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>
+                           <Button />
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
-     );
+    );
 }
- 
+
 export default PatientLog;
