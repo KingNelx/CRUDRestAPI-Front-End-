0beee87b3ef8d4f013c9cd98655057663d174f87
KingNelx <tapiajonel606@gmail.com>
2022-11-04 08:28:45 +0800

    added css for doc pages

--- src/Patients/PatientLog.jsx
+++ src/Patients/PatientLog.jsx
@@ -1,7 +1,24 @@
 const PatientLog = () => {
     return ( 
-        <div>
-            
+        <div className="container patient-top-head">
+            <table className="table-main">
+                <thead>
+                    <tr className="table-head">
+                        <th>#</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Contact Number</th>
+                        <th>RHU</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <th>1</th>
+                    <td>Jonel</td>
+                    <td>Tapia</td>
+                    <td>09071205892</td>
+                    <td>Bato</td>
+                </tbody>
+            </table>
         </div>
      );
 }
